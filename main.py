358b1ce3e97a0480ad4a84bc7e246178d3e2ccb6
Sahil3458Q <sahilshaikh13a@gmail.com>
2025-10-26 04:34:53 +0000

    Update main.py
    zigzag-string condersion

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import sqlite3
+word =input()
+colrow = int(input())
+result = []
+constant = (colrow-2)*2
+cn = 1
+if colrow >= 3:
+    n = (colrow-3)*2 + 4
+    result+= [word[k] for k in range(len(word)) if k%n==0]
+    while constant >= 0:
+        r = [word[k] for k in range(cn,len(word)) if ((k-cn)%n == 0 or n==0) and k < len(word)]
+        l = [word[k+constant] for k in range(cn,len(word)) if ((k-cn)%n == 0 or n==0) and k+constant < len(word)]
+        for k in range(max(len(r),len(l))):
+            try :
+                result.append(r[k])
+            except:
+                pass
+            try : 
+                result.append(l[k])
+            except:
+                pass
+        cn +=1
+        constant-=2
+        if word[cn] == word[cn+constant] :
+            break
+        
+    result+= list(word[colrow-1::((colrow-1)*2)])
+    print("".join(result))
 
-conn = sqlite3.connect("nothing.db")
-c = conn.cursor()
+elif colrow == 2 :
+    result+=[word[k] for k in range(len(word)) if k%2 == 0]
+    result+=[word[k] for k in range(len(word)) if k%2 != 0]
+    print(" ".join(result))
 
-c.execute("""UPDATE chart SET first_name = 'Bob' WHERE rowid = 6 """)
+else:
+    print(word if colrow == 1 else "Wrong no. of rows given.")
 
-c.execute("SELECT * FROM chart")
-
-print(c.fetchall())
-
-conn.commit()
-conn.close()
